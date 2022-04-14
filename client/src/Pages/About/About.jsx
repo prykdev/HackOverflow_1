8155534b7410884d283f35ba66af05928551b577
prykdev <priyankaafssulur@gmail.com>
2022-04-14 08:23:17 +0530

    added about

--- client/src/Pages/About/About.jsx
+++ client/src/Pages/About/About.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 import NavbarComponent from "../../Components/Navbar/NavbarComponent"
 import "./About.scss"
+import eren from "./eren.png"
+import priyanka from "./priyanka.png"
+import ayush from "./ayush.png"
 import { AboutCard } from "../../Components/AboutCard/AboutCard"
 const About = () => {
   return (
@@ -14,6 +17,7 @@ const About = () => {
           github='https://github.com/tgoyal63'
           email=''
           twitter='https://twitter.com/tgoyal63'
+          img={eren}
         />
         <AboutCard
           name='Priyanka Prasad'
@@ -22,6 +26,7 @@ const About = () => {
           github='https://github.com/prykdev'
           email='priyankaafssulur@gmail.com'
           twitter='https://twitter.com/prykdev'
+          img={priyanka}
         />
         <AboutCard
           name='Ayush Singla'
@@ -30,6 +35,7 @@ const About = () => {
           github='https://github.com/ayu913'
           email=''
           twitter='https://twitter.com/ayu913'
+          img={ayush}
         />
       </div>
     </div>
